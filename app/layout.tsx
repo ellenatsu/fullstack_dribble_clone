08080b46e6f04e2caee182ae4fbd737d7cf3650d
Ellen/Zhixuan <ellenatsuwang@gmail.com>
2023-10-10 13:13:05 -0700

    init and add basic files

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Flexibble",
+  description: "Dribble clone by Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
