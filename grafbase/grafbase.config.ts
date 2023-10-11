eecd94846bc82f4689fb3fa6150e2192a8723c86
Ellen/Zhixuan <ellenatsuwang@gmail.com>
2023-10-11 12:50:29 -0700

    revise and add params

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,4 +1,4 @@
-import { g, auth, config } from '@grafbase/sdk'
+import { g, auth, config } from "@grafbase/sdk";
 
 // const authProvider = auth.OpenIDConnect({
 //   issuer: process.env.ISSUER_URL ?? ''
@@ -8,41 +8,29 @@ import { g, auth, config } from '@grafbase/sdk'
 // https://grafbase.com/docs/database
 
 // @ts-ignore
-const User = g.model('User', {
+const User = g.model("User", {
   name: g.string().length({ min: 2, max: 100 }),
   email: g.string().unique(),
   avatarUrl: g.url(),
   description: g.string().length({ min: 2, max: 1000 }).optional(),
   githubUrl: g.url().optional(),
-  linkedinUrl: g.url().optional(), 
-  projects: g.relation(() => Project).list().optional(),
-}).auth((rules) => {
-  rules.public().read()
-})
+  linkedinUrl: g.url().optional(),
+  projects: g
+    .relation(() => Project)
+    .list()
+    .optional(),
+});
 
 // @ts-ignore
-const Project = g.model('Project', {
+const Project = g.model("Project", {
   title: g.string().length({ min: 3 }),
-  description: g.string(), 
+  description: g.string(),
   image: g.url(),
-  liveSiteUrl: g.url(), 
-  githubUrl: g.url(), 
-  category: g.string().search(),
-  createdBy: g.relation(() => User),
-}).auth((rules) => {
-  rules.public().read()
-  rules.private().create().delete().update()
-})
-
-const jwt = auth.JWT({
-  issuer: 'grafbase',
-  secret:  g.env('NEXTAUTH_SECRET')
-})
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string(),
+});
 
 export default config({
   schema: g,
-  auth: {
-    providers: [jwt],
-    rules: (rules) => rules.private()
-  },
-})
+});
