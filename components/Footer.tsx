8490884f1ece1d169f6c8dcae27c6288a8a3a734
Ellen/Zhixuan <ellenatsuwang@gmail.com>
2023-10-11 09:13:44 -0700

    add grafbase

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -68,7 +68,10 @@ const Footer = () => {
           />
         </div>
 
-        <div></div>
+        <div className="flexBetween footer_copyright">
+          <p>@2023 Flexibble. All rights reserved</p>
+          <p className="text-gray"><span className="text-black font-semibold">10,214</span>projects submitted</p>
+        </div>
       </div>
     </footer>
   );
