711660de6e7e22ae88d0ef6877a7b0e69d7d6485
Ellen/Zhixuan <ellenatsuwang@gmail.com>
2023-11-02 21:57:55 -0700

    revise category

--- constants/index.ts
+++ constants/index.ts
@@ -1,22 +1,15 @@
 export const NavLinks = [
   { href: "/", key: "Inspiration", text: "Inspiration" },
   { href: "/", key: "Find Projects", text: "Find Projects" },
-  { href: "/", key: "Learn Development", text: "Learn Development" },
-  { href: "/", key: "Career Advancement", text: "Career Advancement" },
-  { href: "/", key: "Hire Developers", text: "Hire Developers" },
 ];
 
 export const categoryFilters = [
-  "Frontend",
-  "Backend",
-  "Full-Stack",
   "ArtWork",
-  "Mobile",
   "UI/UX",
   "Game Dev",
-  "DevOps",
-  "Data Science",
-  "Machine Learning",
+  "Mobile",
+  "Full-Stack",
+  "Product"
 ];
 
 export const footerLinks = [
